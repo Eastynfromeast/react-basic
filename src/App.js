dc64aa7bfe9e6c0ed3edc85c6df51f3c2eb14492
Eastyn <eastynwork@gmail.com>
2024-06-18 17:39:28 +0900

    useEffect is a React Hook that lets you synchronize a component with an external system

--- src/App.js
+++ src/App.js
@@ -1,12 +1,30 @@
-import Button from "./Button";
-import styles from "./App.module.css";
+import { useEffect, useState } from "react";
+
 function App() {
+	const [value, setValue] = useState(0);
+	const onClick = () => setValue(prev => prev + 1);
+	console.log("i run all the time");
+
+	useEffect(() => {
+		console.log("call the api");
+	}, []);
 	return (
 		<div>
-			<h1 className={styles.title}>React for beginner</h1>
-			<Button text={"Continue"} />
+			<h1>{value}</h1>
+			<button onClick={onClick}>click me</button>
 		</div>
 	);
 }
 
 export default App;
+
+/*
+    Effects
+        sometimes you want to be able to run some code only on the first render
+        when your state changed all the code will be running again 
+        
+        How can we limit rendering of some code? 
+        => useEffect
+            useEffect is a React Hook that lets you synchronize a component with an external system.
+
+*/
