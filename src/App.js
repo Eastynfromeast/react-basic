9e9ee7a6eec4c6867cae4efcfe0c809ec8789ddd
Eastyn <eastynwork@gmail.com>
2024-06-19 18:20:42 +0900

    create an calculator.html for an assignment of the challenge

--- src/App.js
+++ src/App.js
@@ -8,37 +8,41 @@ function App() {
 	const onChangeAmount = event => setAmount(event.target.value);
 
 	const onSelect = e => setValue(e.target.value);
-	useEffect(() => {
+	/* useEffect(() => {
 		fetch("https://api.coinpaprika.com/v1/tickers")
 			.then(response => response.json())
 			.then(json => {
 				setCoins(json);
 				setIsLoading(false);
 			});
-	}, []);
+	}, []); */
 
 	return (
 		<div>
-			<h1>The Coin Tracker {isLoading ? "" : `(${coins.length})`}</h1>
-			{isLoading ? (
-				<h3>loading...</h3>
-			) : (
+			{isLoading ? null : (
 				<>
-					<select onSelect={onSelect}>
-						{coins.map((coin, index) => {
-							return (
-								<option value={coin.quotes.USD.price} key={coin.id}>
-									{coin.name}({coin.symbol}) : {coin.quotes.USD.price}
-								</option>
-							);
-						})}
-					</select>
-					<hr />
-					<div>
-						<label htmlFor="amount">Number?</label>
-						<input value={amount} onChange={onChangeAmount} type="number" id="amount" placeholder="Put any number..." />
-						<p>{value}</p>
-					</div>
+					<h1>The Coin Tracker {isLoading ? "" : `(${coins.length})`}</h1>
+					{isLoading ? (
+						<h3>loading...</h3>
+					) : (
+						<>
+							<select onSelect={onSelect}>
+								{coins.map((coin, index) => {
+									return (
+										<option value={coin.id} key={coin.id}>
+											{coin.name}({coin.symbol}) : {coin.quotes.USD.price}
+										</option>
+									);
+								})}
+							</select>
+							<hr />
+							<div>
+								<label htmlFor="amount">Number?</label>
+								<input value={amount} onChange={onChangeAmount} type="number" id="amount" placeholder="Put any number..." />
+								<p>{value}</p>
+							</div>
+						</>
+					)}
 				</>
 			)}
 		</div>
